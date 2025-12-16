fe38a7ab852f84821038988234396a63c62834d0
chagnetl <lucas.chagnet@etu.univ-smb.fr>
2025-12-16 17:36:06 +0100

    Changement nom train

--- TRAINBattle/UCChoixPerso.xaml.cs
+++ TRAINBattle/UCChoixPerso.xaml.cs
@@ -45,7 +45,7 @@ namespace TRAINBattle
             {
                 new PersoChoix { Id = 0, Nom = "Zach", ImageProfil = "/img/train1/profil.png" },
                 new PersoChoix { Id = 1, Nom = "ミク", ImageProfil = "/img/train2/profil.png" },
-                new PersoChoix { Id = 2, Nom = "Robin", ImageProfil = "/img/train3/profil.png" },
+                new PersoChoix { Id = 2, Nom = "Série 4", ImageProfil = "/img/train3/profil.png" },
                 new PersoChoix { Id = 3, Nom = "Abdelaziz", ImageProfil = "/img/train4/profil.png" },
             };
             MainWindow.IndexPersoP1 = choixP1;
